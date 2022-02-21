400d61557b5d7a439e1f81fb0e4a8f0c51396ef9
facundo <facuwilli@gmail.com>
2022-02-21 20:49:38 -0300

    Added option class.

---
+++ Z-Apocalipse/src/game/options.cpp
@@ -0,0 +1,39 @@
+#include "options.h"
+
+namespace Z_APOCALIPS 
+{
+	Options::Options() 
+	{
+
+	}
+
+	Options::~Options() 
+	{
+
+	}
+
+	void init()
+	{
+
+	}
+	
+	void input() 
+	{
+
+	}
+	
+	void update() 
+	{
+
+	}
+	
+	void draw() 
+	{
+
+	}
+	
+	void deinit()
+	{
+
+	}
+}
---
+++ Z-Apocalipse/src/game/options.h
@@ -0,0 +1,22 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+namespace Z_APOCALIPS 
+{
+	class Options
+	{
+	private:
+
+	public:
+		Options();
+		~Options();
+
+		void init();
+		void input();
+		void update();
+		void draw();
+		void deinit();
+	};
+}
+
+#endif
