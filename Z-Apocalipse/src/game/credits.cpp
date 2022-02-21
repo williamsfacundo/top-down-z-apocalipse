e81c5aa340099ede661e90c38e8e31c8f7a4ce7d
facundo <facuwilli@gmail.com>
2022-02-21 20:43:34 -0300

    Added credits class.

---
+++ Z-Apocalipse/src/game/credits.cpp
@@ -0,0 +1,39 @@
+#include "credits.h"
+
+namespace Z_APOCALIPS 
+{
+	Credits::Credits() 
+	{
+
+	}
+
+	Credits::~Credits() 
+	{
+
+	}
+
+	void Credits::init() 
+	{
+
+	}
+	
+	void Credits::input() 
+	{
+
+	}
+	
+	void Credits::update() 
+	{
+
+	}
+	
+	void Credits::draw() 
+	{
+
+	}
+	
+	void Credits::deinit() 
+	{
+
+	}
+}
---
+++ Z-Apocalipse/src/game/credits.h
@@ -0,0 +1,22 @@
+#ifndef CREDITS_H
+#define CREDITS_H
+
+namespace Z_APOCALIPS 
+{
+	class Credits
+	{
+	private:
+
+	public:
+		Credits();
+		~Credits();
+
+		void init();
+		void input();
+		void update();
+		void draw();
+		void deinit();
+	};
+}
+
+#endif
