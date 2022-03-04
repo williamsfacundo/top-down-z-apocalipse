a7153484378387930bfacbbf4a35dffa6779028c
facundo <facuwilli@gmail.com>
2022-03-04 20:29:19 -0300

    Added end game class.
    For now the class has a button to go back to menu.

---
+++ Z-Apocalipse/src/gameplay/end_game.cpp
@@ -0,0 +1,36 @@
+#include "end_game.h"
+
+#include <raylib.h>
+
+#include "..\game\button.h"
+#include "scene_manager.h"
+
+namespace Z_APOCALIPSE 
+{
+	EndGame::EndGame() 
+	{
+		goMenuButton = new Button({ static_cast<float>(GetScreenWidth() * goMenuButtonYPercentage),
+			static_cast<float>(GetScreenHeight() * goMenuButtonYPercentage), static_cast<float>(GetScreenWidth() * goMenuButtonWidthPercentage),
+			static_cast<float>(GetScreenHeight() * goMenuButtonHeightPercentage) },buttonsColorOne, buttonsColorTwo, goMenuButtonText);
+	}
+
+	EndGame::~EndGame() 
+	{
+		delete goMenuButton;
+	}
+
+	void EndGame::input(SceneManager* sceneManager) 
+	{
+		goMenuButton->changeSceneWhenButtonPress(sceneManager, menuScene);
+	}
+
+	void EndGame::draw() 
+	{
+		BeginDrawing();
+		ClearBackground(backgroundColor);
+
+		goMenuButton->draw();
+
+		EndDrawing();
+	}
+}
---
+++ Z-Apocalipse/src/gameplay/end_game.h
@@ -0,0 +1,38 @@
+#ifndef END_GAME_H
+#define END_GAME_H
+
+#include <raylib.h>
+
+#include "..\game\button.h"
+#include "scene_manager.h"
+
+namespace Z_APOCALIPSE 
+{
+	class EndGame
+	{
+	private:
+		Scenes menuScene = Scenes::MAIN_MENU;
+		Scenes gameplayScene = Scenes::GAMEPLAY;
+
+		const Color backgroundColor = BLACK;
+		const Color buttonsColorOne = YELLOW;
+		const Color buttonsColorTwo = RED;
+
+		const float goMenuButtonYPercentage = 0.3f;
+		const float goMenuButtonWidthPercentage = 0.2f;
+		const float goMenuButtonHeightPercentage = 0.1f;
+		const float goMenuButtonYPercentage = 0.9f;
+
+		const char* goMenuButtonText = "GO MENU";
+
+		Button* goMenuButton;
+	public:
+		EndGame();
+		~EndGame();
+
+		void input(SceneManager* sceneManager);
+		void draw();
+	};
+}
+
+#endif
