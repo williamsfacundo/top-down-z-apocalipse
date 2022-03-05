56000b03988f73c8861a4f16294a7170b73b6a1e
facundo <facuwilli@gmail.com>
2022-03-04 21:17:42 -0300

    Added restart button in end game scene.

--- Z-Apocalipse/src/gameplay/end_game.cpp
+++ Z-Apocalipse/src/gameplay/end_game.cpp
@@ -10,18 +10,25 @@ namespace Z_APOCALIPSE
 	EndGame::EndGame() 
 	{
 		goMenuButton = new Button({ static_cast<float>(GetScreenWidth() * goMenuButtonXPercentage),
-			static_cast<float>(GetScreenHeight() * goMenuButtonYPercentage), static_cast<float>(GetScreenWidth() * goMenuButtonWidthPercentage),
-			static_cast<float>(GetScreenHeight() * goMenuButtonHeightPercentage) },buttonsColorOne, buttonsColorTwo, goMenuButtonText);
+			static_cast<float>(GetScreenHeight() * buttonsYPercentage), static_cast<float>(GetScreenWidth() * buttonsWidthPercentage),
+			static_cast<float>(GetScreenHeight() * buttonsHeightPercentage) },buttonsColorOne, buttonsColorTwo, goMenuButtonText);
+		
+		restartButton = new Button({ static_cast<float>(GetScreenWidth() * restartButtonXPercentage),
+			static_cast<float>(GetScreenHeight() * buttonsYPercentage), static_cast<float>(GetScreenWidth() * buttonsWidthPercentage),
+			static_cast<float>(GetScreenHeight() * buttonsHeightPercentage) }, buttonsColorOne, buttonsColorTwo, restartButtonText);
 	}
 
 	EndGame::~EndGame() 
 	{
 		delete goMenuButton;
+		delete restartButton;
 	}
 
 	void EndGame::input(SceneManager* sceneManager) 
 	{
 		goMenuButton->changeSceneWhenButtonPress(sceneManager, menuScene);
+
+		restartButton->changeSceneWhenButtonPress(sceneManager, gameplayScene);
 	}
 
 	void EndGame::draw() 
@@ -31,6 +38,16 @@ namespace Z_APOCALIPSE
 
 		goMenuButton->draw();
 
+		restartButton->draw();
+
+		drawTitle();
+
 		EndDrawing();
+	}
+
+	void EndGame::drawTitle() 
+	{
+		DrawText(titleText, static_cast<int>(GetScreenWidth() * titleXPercentage),
+			static_cast<int>(GetScreenHeight() * titleYPercentage), titleSize, titleColor);
 	}
 }
--- Z-Apocalipse/src/gameplay/end_game.h
+++ Z-Apocalipse/src/gameplay/end_game.h
@@ -14,24 +14,35 @@ namespace Z_APOCALIPSE
 		Scenes menuScene = Scenes::MAIN_MENU;
 		Scenes gameplayScene = Scenes::GAMEPLAY;
 
+		const Color titleColor = RED;
 		const Color backgroundColor = BLACK;
 		const Color buttonsColorOne = YELLOW;
 		const Color buttonsColorTwo = RED;
 
-		const float goMenuButtonXPercentage = 0.2f;
-		const float goMenuButtonWidthPercentage = 0.2f;
-		const float goMenuButtonHeightPercentage = 0.1f;
-		const float goMenuButtonYPercentage = 0.9f;
+		const float titleYPercentage = 0.3f;
+		const float titleXPercentage = 0.26f;
+		const float goMenuButtonXPercentage = 0.1f;
+		const float restartButtonXPercentage = 0.7f;
+		const float buttonsWidthPercentage = 0.2f;
+		const float buttonsHeightPercentage = 0.1f;
+		const float buttonsYPercentage = 0.8f;
 
+		const short titleSize = 60;
+
+		const char* titleText = "END GAME";
 		const char* goMenuButtonText = "GO MENU";
+		const char* restartButtonText = "RESTART";
 
 		Button* goMenuButton;
+		Button* restartButton;
 	public:
 		EndGame();
 		~EndGame();
 
 		void input(SceneManager* sceneManager);
 		void draw();
+
+		void drawTitle();
 	};
 }
 
